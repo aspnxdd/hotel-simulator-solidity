d57a071c081628bdb44c7487be278f1778388c0e
aspnxdd <43625217+aspnxdd@users.noreply.github.com>
2021-11-19 23:14:18 +0100

    tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,27 @@
-
 module.exports = {
 
-  purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  purge: {
+    content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+    safelist: [
+      'sidebar-icon ',
+      'sidebar-icon-bottom',
+      'sidebar-tooltip'
+    ]
+  },
   mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
-    
+
     extend: {
-      fontFamily:{
-        ShipporiAntique:["Shippori Antique","sans-serif"],
-        Nunito:["Nunito", "sans-serif"]
+      fontFamily: {
+        ShipporiAntique: ["Shippori Antique", "sans-serif"],
+        Nunito: ["Nunito", "sans-serif"]
       },
       colors: {
         primary: "#4e4e4e",
-        maticColor:"#8247e5",
-        maticColorHover:"#b48ff2"
-        
+        maticColor: "#8247e5",
+        maticColorHover: "#b48ff2"
+
       }
     },
   },
