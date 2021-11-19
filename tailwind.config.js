30fd18ecb6454e99ce962f13fbb33b23aa663e57
aspnxdd <43625217+aspnxdd@users.noreply.github.com>
2021-11-19 22:50:04 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // const colors = require("tailwindcss/colors")
 
 module.exports = {
-  purge: ['./pages/**/*.{js}', './components/**/*.{js}'],
+  purge:  {},
   mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
