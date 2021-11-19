369fd1cdb92c64e36628a05458b7a40c0b2dfca2
aspnxdd <43625217+aspnxdd@users.noreply.github.com>
2021-11-19 22:37:12 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  // mode: "jit",
+  mode: "",
   darkMode: false, // or 'media' or 'class'
   theme: {
     
