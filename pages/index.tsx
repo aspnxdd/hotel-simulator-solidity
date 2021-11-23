7825f60a5ed62ec4a7163a29fc69d8f01e07e56c
aspnxdd <43625217+aspnxdd@users.noreply.github.com>
2021-11-23 21:08:17 +0100

    md

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center md:w-auto overflow-x-hidden">
-      <h1 className="text-6xl font-bold text-gray-700 drop-shadow-2xl text-center md:text-indigo-300">
+    <div className="flex flex-col items-center justify-center overflow-x-hidden">
+      <h1 className="text-4xl font-bold -mr-20 text-indigo-400 drop-shadow-2xl text-center md:text-6xl md:-mr-0">
         Hotel simulator
       </h1>
 
       <hr className="w-4/12 mt-4 mb-2 border-t md:w-auto" />
-      <h1 className="text-base italic text-gray-400 drop-shadow-2xl -ml-24 md:w-auto">
+      <h1 className="text-base italic text-gray-400 drop-shadow-2xl ml-24 md:-ml-24">
         Built on the Matic (Polygon) testnet (Mumbai)
       </h1>
       <div className="flex items-center justify-center animation-box-left space-x-32 md:w-auto">
@@ -33,20 +33,20 @@ const Home: NextPage = () => {
           </a>
         </Link>
       </div>
-      <p className="w-2/4 mt-14">
+      <p className="w-3/4 ml-20 mt-14 md:w-2/4 md:ml-0">
         Welcome to the Hotel Simulator built on the polygon testnet blockchain.
         This website puts forth in the adoption of smart contracts in the
         traditional business like an hotel. in order to interact with this dapp
         (decentralized app) you must have installed Metamask and set up the
         polygon testnet (mumbai) in it.
       </p>
-      <p className="w-2/4 mt-10">
+      <p className="w-3/4 ml-20 mt-14 md:w-2/4 md:ml-0">
         It tries to simulate an hotel booking portal for mutliple hotel owners,
         in which they can create an instance of the smart contract that sets up
         an hotel with its most basic features such as its name and the number of
         rooms it has.
       </p>
-      <p className="w-2/4 mt-10">
+      <p className="w-3/4 ml-20 mt-14 md:w-2/4 md:ml-0">
         Every hotel owner receives the price of the booked room(s) right when a
         room is booked. When a new hotel is created, I receive a fee as being
         the owner of this portal.
